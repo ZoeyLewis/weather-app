5ddac4c3043e4599bc3d24575a6e7ffd218fc56c
ZoeyLewis <89399185+ZoeyLewis@users.noreply.github.com>
2021-09-29 13:44:03 +0100

    Uploaded Weather App HTML, CSS and JS.

---
+++ src/src.js
@@ -0,0 +1,112 @@
+let currentDateTime = document.querySelector("#today-date");
+let now = new Date();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let weekDay = days[now.getDay()];
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+let month = months[now.getMonth()];
+let date = now.getDate();
+let year = now.getFullYear();
+let hour = now.getHours();
+if (hour < 10) {
+  hour = `0${hour}`;
+}
+let minute = now.getMinutes();
+if (minute < 10) {
+  minute = `0${minute}`;
+}
+currentDateTime.innerHTML = `${weekDay}  ${month}  ${date}  ${year}  ${hour}:${minute}`;
+
+function changeCityName(event) {
+  event.preventDefault();
+  let searchValue = document.querySelector("#city-search-input");
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = `${searchValue.value.toUpperCase()}`;
+}
+
+function changeCelciusTempValue(response) {
+  let temperature = Math.round(response.data.main.temp);
+  let tempValue = document.querySelector("#degrees");
+  tempValue.innerHTML = temperature;
+  let weatherDescription = document.querySelector("#today-temp-tag");
+  weatherDescription.innerHTML =
+    response.data.weather[0].description.toUpperCase();
+}
+
+function changeFahrenheitTempValue(response) {
+  let temperature = Math.round(response.data.main.temp);
+  let tempValue = document.querySelector("#degrees");
+  tempValue.innerHTML = temperature;
+  let weatherDescription = document.querySelector("#today-temp-tag");
+  weatherDescription.innerHTML =
+    response.data.weather[0].description.toUpperCase();
+}
+function getCelciusWeatherAPI(event) {
+  let city = document.querySelector("#city-search-input").value;
+  let apiKey = `eb9c72edfe9b2e4dc537230ab5404717`;
+  let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+  axios.get(apiURL).then(changeCelciusTempValue);
+}
+function getFahrenheitWeatherAPI(event) {
+  let city = document.querySelector("#city-search-input").value;
+  let apiKey = `eb9c72edfe9b2e4dc537230ab5404717`;
+  let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+  axios.get(apiURL).then(changeFahrenheitTempValue);
+}
+
+let citySearch = document.querySelector("#city-form");
+citySearch.addEventListener("submit", changeCityName);
+citySearch.addEventListener("submit", getCelciusWeatherAPI);
+
+let celciusButton = document.querySelector("#celcius");
+celciusButton.addEventListener("click", getCelciusWeatherAPI);
+
+let fahrenheightButton = document.querySelector("#fahrenheight");
+fahrenheightButton.addEventListener("click", getFahrenheitWeatherAPI);
+
+function showCurrentLocationWeather(response) {
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = response.data.name.toUpperCase();
+  let temperature = Math.round(response.data.main.temp);
+  let tempValue = document.querySelector("#degrees");
+  tempValue.innerHTML = temperature;
+  let weatherDescription = document.querySelector("#today-temp-tag");
+  weatherDescription.innerHTML =
+    response.data.weather[0].description.toUpperCase();
+}
+
+function getAPILocation(position) {
+  let apiKey = `eb9c72edfe9b2e4dc537230ab5404717`;
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let apiURL = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${apiKey}`;
+  axios.get(apiURL).then(showCurrentLocationWeather);
+}
+
+function getLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(getAPILocation);
+}
+let locationButton = document.querySelector("#location-button");
+locationButton.addEventListener("click", getLocation);
